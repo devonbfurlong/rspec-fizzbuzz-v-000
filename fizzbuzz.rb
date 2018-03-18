e476ca44074a75a4be84509ce2359bafd5bf4c0d
Devon Furlong <devonbfurlong@gmail.com>
2018-03-18 19:32:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,13 @@
 def fizzbuzz(int)
-  if int % 5 == 0
-      "Buzz"
-    end
-    if int % 3 == 0
+  if int % 3 == 0
       "Fizz"
     end
     if int % 5 == 0
         "Buzz"
       end
+      if int % 5 == 0 && int % 3 == 0 
+        "FizzBuzz"
+      end
   end
+  
+    
